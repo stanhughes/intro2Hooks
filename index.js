4fa9179132cc8be3484f057f3f13c4ed1e72c037
Stan Hughes <hughes.stan@gmail.com>
2019-05-27 13:42:14 -0400

    Added files that need to be tracked.

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+ // Import
+import React, { useState }  from 'react'
+import { AppRegistry, Text, View, TouchableOpacity } from 'react-native'
+
+// Create a component
+const App = () => {
+    const [count, setCount] = useState(0);
+
+    return (
+        <View style={ {justifyContent: 'center', alignItems: 'center', flex: 1} }>
+            <TouchableOpacity onPress={() => setCount(count + 1)}>
+                <Text style={ {fontSize:30} }> Click me {count} times </Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+// Render it to the device
+AppRegistry.registerComponent('intro2Hooks', () => App)
